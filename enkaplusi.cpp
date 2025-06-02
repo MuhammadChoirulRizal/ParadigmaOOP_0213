ea7e9ddc282c9f6bb2117e46c66406bea5f8ba85
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-06-02 18:38:41 +0700

    memanggil lampu rumah

--- enkaplusi.cpp
+++ enkaplusi.cpp
@@ -15,3 +15,17 @@ string getSaklarNo(int i)
     return saklarNo[i];
 }
 };
+int main()
+{
+    remoteLampu lampuRumah;
+    lampuRumah.setSaklarNo(0, "lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "lampu ruang tamu");
+    lampuRumah.setSaklarNo(2, "lampu kamar tidur");
+    lampuRumah.setSaklarNo(3, "lampu dapur");
+    
+    cout << lampuRumah.getSaklarNo(0)<< endl;
+    cout << lampuRumah.getSaklarNo(1)<< endl;
+    cout << lampuRumah.getSaklarNo(2)<< endl;
+    cout << lampuRumah.getSaklarNo(3)<< endl;
+    return 0;
+}
